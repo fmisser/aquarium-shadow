b031d863e1135aeeb9ab489e4eb17c87e0f68fa8
fmisser-workpc <fmisser@vip.qq.com>
2016-02-24 16:23:18 +0800

    Initial commit

---
+++ src/main/java/com/fmisser/AquariumShadowApplication.java
@@ -0,0 +1,12 @@
+package com.fmisser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquariumShadowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquariumShadowApplication.class, args);
+	}
+}
